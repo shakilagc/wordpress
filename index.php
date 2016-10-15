e41fd01911ee09e55fc5b822666adbb0b7710df8
shakilagc <shakil@acquaintsoft.com>
2016-10-15 18:48:45 +0530

    changed in index

--- index.php
+++ index.php
@@ -12,6 +12,6 @@
  * @var bool
  */
 define('WP_USE_THEMES', true);
-echo 333;
+echo 111;
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
